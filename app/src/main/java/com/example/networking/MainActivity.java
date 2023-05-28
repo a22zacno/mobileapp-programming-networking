5629255fb1425078c4f3f726f16c1d94d42f0d69
a22zacno <a22zano@studnet.his.se>
2023-05-28 23:38:50 +0200

    added new jsontask in main. gave internet permission added json url

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
     @Override
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
